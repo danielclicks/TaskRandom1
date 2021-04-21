9b2919cb383727649b60ab16a0c3e553c624b660
danielclicks <ordoniocharles@gmail.com>
2021-04-21 14:27:25 +0800

    Update RandomNum.sh

--- RandomNum.sh
+++ RandomNum.sh
@@ -12,12 +12,14 @@
 #   Description:  Writes number randomly and show one at a time
 #   Limitations:  The script limits the number from 1-10
 #   Bugs       :  The number will show twice after the it loops from 1-10
-# 
+#   Tests      :  jdoodle.com/a/3e5r
 #   
-#
+#   Instruction: Random function will run random number and will limit the number from 1-10
+#         It will increment the number to 1 and the variable number will produce number from 1-10 one at a time.
 #
 #---------------------------------------------------------------------------------
 ##################################################################################
+
 
 number=$(( ( $RANDOM % 10 ) + 1 ))
 echo "$number" #To produce random number at a time from 1-10
